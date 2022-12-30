fdf46debadab6debacc45e18f9becc7f6152827a
Coda Soda <theothercoda@gmail.com>
2022-12-29 20:45:43 -0500

    LineEmojiDownloader v1

---
+++ LineEmojiDownloader/EmojiDownloaderForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace LineEmojiDownloader
+{
+    partial class EmojiDownloaderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmojiDownloaderForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.URLTextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.animatedCheckBox = new System.Windows.Forms.CheckBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.numOfEmojisLabel = new System.Windows.Forms.Label();
+            this.numOfEmojis = new System.Windows.Forms.TextBox();
+            this.directoryButton = new System.Windows.Forms.Button();
+            this.directoryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(523, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter emoji pack URL: (Example: https://store.line.me/emojishop/product/61e2818c2" +
+    "01b4c6cc07b7f21/en )";
+            // 
+            // URLTextbox
+            // 
+            this.URLTextbox.Location = new System.Drawing.Point(12, 36);
+            this.URLTextbox.Name = "URLTextbox";
+            this.URLTextbox.Size = new System.Drawing.Size(520, 20);
+            this.URLTextbox.TabIndex = 1;
+            this.URLTextbox.TextChanged += new System.EventHandler(this.URLTextbox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(457, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // animatedCheckBox
+            // 
+            this.animatedCheckBox.AutoSize = true;
+            this.animatedCheckBox.Location = new System.Drawing.Point(12, 91);
+            this.animatedCheckBox.Name = "animatedCheckBox";
+            this.animatedCheckBox.Size = new System.Drawing.Size(137, 17);
+            this.animatedCheckBox.TabIndex = 3;
+            this.animatedCheckBox.Text = "Get animated versions?";
+            this.animatedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(12, 157);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(39, 13);
+            this.OutputLabel.TabIndex = 4;
+            this.OutputLabel.Text = "Output";
+            // 
+            // numOfEmojisLabel
+            // 
+            this.numOfEmojisLabel.AutoSize = true;
+            this.numOfEmojisLabel.Location = new System.Drawing.Point(54, 68);
+            this.numOfEmojisLabel.Name = "numOfEmojisLabel";
+            this.numOfEmojisLabel.Size = new System.Drawing.Size(95, 13);
+            this.numOfEmojisLabel.TabIndex = 5;
+            this.numOfEmojisLabel.Text = "How many emojis?";
+            // 
+            // numOfEmojis
+            // 
+            this.numOfEmojis.Location = new System.Drawing.Point(12, 65);
+            this.numOfEmojis.Name = "numOfEmojis";
+            this.numOfEmojis.Size = new System.Drawing.Size(36, 20);
+            this.numOfEmojis.TabIndex = 6;
+            // 
+            // directoryButton
+            // 
+            this.directoryButton.Location = new System.Drawing.Point(12, 115);
+            this.directoryButton.Name = "directoryButton";
+            this.directoryButton.Size = new System.Drawing.Size(137, 23);
+            this.directoryButton.TabIndex = 7;
+            this.directoryButton.Text = "Choose install directory";
+            this.directoryButton.UseVisualStyleBackColor = true;
+            this.directoryButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // directoryLabel
+            // 
+            this.directoryLabel.AutoSize = true;
+            this.directoryLabel.Location = new System.Drawing.Point(155, 120);
+            this.directoryLabel.Name = "directoryLabel";
+            this.directoryLabel.Size = new System.Drawing.Size(115, 13);
+            this.directoryLabel.TabIndex = 8;
+            this.directoryLabel.Text = "No install path chosen!";
+            // 
+            // EmojiDownloaderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 179);
+            this.Controls.Add(this.directoryLabel);
+            this.Controls.Add(this.directoryButton);
+            this.Controls.Add(this.numOfEmojis);
+            this.Controls.Add(this.numOfEmojisLabel);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.animatedCheckBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.URLTextbox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EmojiDownloaderForm";
+            this.Text = "Line Emoji Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox URLTextbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox animatedCheckBox;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label numOfEmojisLabel;
+        private System.Windows.Forms.TextBox numOfEmojis;
+        private System.Windows.Forms.Button directoryButton;
+        private System.Windows.Forms.Label directoryLabel;
+    }
+}
+
---
+++ LineEmojiDownloader/EmojiDownloaderForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LineEmojiDownloader
+{
+    public partial class EmojiDownloaderForm : Form
+    {
+        public EmojiDownloaderForm()
+        {
+            InitializeComponent();
+            this.directoryButton.Click += new System.EventHandler(this.BrowseFolderButton_Click);
+        }
+
+        private void EmojiDownloaderForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void URLTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.OutputLabel.Text = "Output";
+
+            string emojiID;
+
+            if (!(this.URLTextbox.Text.Length > 0) || !(this.URLTextbox.Text.Contains("/")))
+            {
+                this.OutputLabel.Text = "URL could not be parsed, HexID for emojipack could not be found.";
+                return;
+            }
+
+            var split = this.URLTextbox.Text.Split('/');
+            emojiID = split[split.Length - 2];
+
+            if (!IsHex(emojiID))
+            {
+                this.OutputLabel.Text = "Found emojiID: " + emojiID + "could not be parsed as a hex value.";
+                return;
+            }
+
+            if (!IsDigitsOnly(this.numOfEmojis.Text))
+            {
+                this.OutputLabel.Text = "Whole numbers only for number of emojis to download.";
+                return;
+            }
+
+            if (!directoryLabel.Text.Contains(":"))
+            {
+                this.OutputLabel.Text = "Please choose an install directory.";
+                return;
+            }
+
+
+            int numOfEmojis = int.Parse(this.numOfEmojis.Text);
+            string newURL = "https://stickershop.line-scdn.net/sticonshop/v1/sticon/" + emojiID + "/iPhone/";
+            string installPath = directoryLabel.Text + "\\" + emojiID + "\\";
+            string fileType = this.animatedCheckBox.Checked ? "_animation.png" : ".png";
+            string format = "000.##";
+
+            if (!Directory.Exists(installPath))
+            {
+                Directory.CreateDirectory(installPath);
+            }
+
+            for (int i = 1; i <= numOfEmojis; i++)
+            {
+                string fileName = (i.ToString(format) + fileType);
+                save_file_from_url((installPath + fileName), (newURL + fileName));
+            }
+
+            this.OutputLabel.Text = "All files downloaded successfully.";
+        }
+
+        public void save_file_from_url(string file_name, string url)
+        {
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
+            byte[] content;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+
+            using (BinaryReader br = new BinaryReader(stream))
+            {
+                content = br.ReadBytes(500000);
+                br.Close();
+            }
+            response.Close();
+
+            FileStream fs = new FileStream(file_name, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            try
+            {
+                bw.Write(content);
+            }
+            finally
+            {
+                fs.Close();
+                bw.Close();
+            }
+        }
+
+        private void BrowseFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog.  
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.directoryLabel.Text = folderDlg.SelectedPath;
+            }
+        }
+
+        private bool IsHex(IEnumerable<char> chars)
+        {
+            bool isHex;
+            foreach (var c in chars)
+            {
+                isHex = ((c >= '0' && c <= '9') ||
+                         (c >= 'a' && c <= 'f') ||
+                         (c >= 'A' && c <= 'F'));
+
+                if (!isHex)
+                    return false;
+            }
+            return true;
+        }
+
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
